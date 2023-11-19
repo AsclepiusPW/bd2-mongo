1ad23b58c0f0522ffa40dafcb9a745f531acaeb0
Marcos Paulo <kingzabitus@gmail.com>
2023-11-19 19:52:06 -0300

    adicionando front-end

---
+++ bd2Projeto2/src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
+
+const ocorrenciaRouter = require('./routes/OcorrenciaRouter');
+app.use('/ocorrencias', ocorrenciaRouter);
+
+
+app.listen(process.env.API_PORT, ()=>{
+    console.log(`API rodando na porta ${process.env.API_PORT}`);
+});
+
