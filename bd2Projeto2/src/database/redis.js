20c913d04ff9fedb101b311ab8ace7fb8f7c90b3
Tobias-Santos <117104950+tobias-santos@users.noreply.github.com>
2023-12-12 21:18:16 -0300

    Adicionando arquivo do banco Redis

---
+++ bd2Projeto2/src/database/redis.js
@@ -0,0 +1,18 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+    password: 'xzJNgF3uTH2UjdyDJvycQa5rVv5vYlKg',
+    socket: {
+        host: 'redis-10374.c308.sa-east-1-1.ec2.cloud.redislabs.com',
+        port: 10374
+    }
+});
+
+const connect = async () => {
+    client.on("error", (err) => console.log("Erro no Redis Client", err));
+    await client.connect();
+  };
+
+connect();
+
+export default client;
